0afa889d9ddd3b16d454d98a6add5f62e55f0f4a
honeyedoasis <honeyedoasis@gmail.com>
2026-03-17 19:27:37 +1100

    Add live vods

--- scrape_json.py
+++ scrape_json.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import yt_dlp
 import time
+import xmltodict
 
 from yt_dlp.extractor.weverse import WeverseIE
 from yt_dlp.utils import ExtractorError
@@ -29,6 +30,7 @@ WRITE_ARTIST_COMMENTS = False
 
 DOWNLOAD_MOMENTS_JSON = True
 DOWNLOAD_MOMENTS_MEDIA = True
+DOWNLOAD_LIVE_VODS = False # this one will take forever to process
 
 # members = {
 #     'jiheon': '5fb309bc7489a576484431ba8338807e',  # jh
@@ -259,12 +261,32 @@ def write_individual_posts(post_file, folder, write_comments=False):
 
     for post_id in posts:
         req = f'/post/v1.0/post-{post_id}?fieldSet=postV1'
-        write_single(req, f'{JSON_FOLDER}/{folder}/{post_id}.json', True)
+        data = write_single(req, f'{JSON_FOLDER}/{folder}/{post_id}.json', True)
+        print(data)
 
         if write_comments:
             print(f'Writing post comments {post_id}')
             req = f'/comment/v1.0/post-{post_id}/comments?fieldSet=postCommentsV1'
             write_paged_requests(req, req, f'{JSON_FOLDER}/{folder}/{post_id}.comments.json', True, True)
+
+        if DOWNLOAD_LIVE_VODS:
+            download_live_vod(data)
+
+def download_live_vod(data):
+    # TODO maybe just use yt-dlp?
+    video_id = data['extension']['video']['videoId']
+    post_id = data['postId']
+    date = data['publishedAt']
+
+    if data['membershipOnly']:
+        print('Skip downloading membership video ', video_id)
+        return
+
+    vod_data = get_vod_video_json(video_id)
+    url = get_vod_url(vod_data)
+    filepath = f'{MEDIA_FOLDER}/vod/{post_id}'
+    print('Downloading vod ', post_id)
+    # utils.download_file(url, filepath, date)
 
 
 def write_live_chat():
@@ -296,10 +318,28 @@ def write_media(community_id):
     write_paged_requests(req, req, MEDIA_JSON, True)
 
 
-def get_video_json(video_id):
-    # whats this for lives?
-    req = f'/video/v2.1/vod/{video_id}/playInfo?version=v2'
-    return write_single(req, f'{JSON_FOLDER}/videos/{video_id}.json', True)
+def get_vod_video_json(video_id):
+    '''
+    '/video/v2.1/vod/75098/playInfo?appId=be4d79eb8fc7bd008ee82c8ec4ff6fd4&communityId=36&gcc=AU&language=en&os=WEB&platform=WEB&playerType=html5_pc&resType=xml&version=v3&wpf=pc&wmd=%2FPk5HfwkvBmU4hspD%2F2u%2BhROCIQ%3D&wmsgpad=1773734785779'
+    '''
+    req = f'/video/v2.1/vod/{video_id}/playInfo?version=v3'
+    return call_request(req)
+
+
+def get_vod_url(data):
+    playback_xml = data['playback']
+
+    data = xmltodict.parse(playback_xml)
+    print(data)
+    # videos = data['videoSource']['videos']['video']
+    types = data['MPD']['Period']['AdaptationSet']
+    adaption_set = next((v for v in types if v['@mimeType'] == 'video/mp4'), None)
+    videos = adaption_set['Representation']
+
+    def video_size(v):
+        return int(v['@bandwidth'])
+
+    return sorted(videos, key=video_size, reverse=True)[0]['BaseURL']
 
 
 def get_cvideo_json(video_id):
@@ -323,32 +363,32 @@ def write_live_tab_posts(community_id):
     req = f'/post/v1.0/community-{community_id}/liveTabPosts'
     write_paged_requests(req, req, LIVE_POSTS_JSON, True)
 
-    if True:
-        print('Bla')
-        # with open(f'{json_location}', 'r', encoding='utf-8') as file:
-        #     json_data = json.load(file)
-        #     for data in json_data:
-        #         postId = data['postId']
-        #         media_info = data['extension']['mediaInfo']
-        #         if chat := media_info.get('chat'):
-        #             posts.append((chat['chatId'], data))
-        #             # artist_msgs = chat['artistMessages']
-        #             # if len(artist_msgs['data']):
-        #             #     posts.append((chat['chatId'], data))
-        #         else:
-        #             print('No chat id?', postId, data['shareUrl'])
-        #             break
-        #
-        # '/chat/v1.0/chat-N1XTf9/messages?after=1733566168468%2C4b0a80d517a7229bf3747c89dc04e457&limit=50'
-        # '/chat/v1.0/chat-N1VwSq/messages?limit=50&appId=be4d79eb8fc7bd008ee82c8ec4ff6fd4&language=en&os=WEB&platform=WEB&wpf=pc&wmsgpad=1736165949156&wmd=gvpNwpjvWqh1sc1eiuqDk4eqeDA%3D'
-        # '/chat/v1.0/chat-N1XTf9/messages?after=1733566168468%2C4b0a80d517a7229bf3747c89dc04e457&limit=50&appId=be4d79eb8fc7bd008ee82c8ec4ff6fd4&language=en&os=WEB&platform=WEB&wpf=pc&wmsgpad=1736165384857&wmd=4CuFyOOaJ7JnTQ0PbDxo5uPDUmI%3D'
-        #
-        # for chatId, data in posts:
-        #     postId = data['postId']
-        #     req = f'/chat/v1.0/chat-{chatId}/messages?limit=1000'
-        #     print(req, postId, data['shareUrl'])
-        #     write_all_requests(req, req, f'{output_folder}/{postId}', True, True)
-        #     # break
+    # if True:
+    # print('Bla')
+    # with open(f'{json_location}', 'r', encoding='utf-8') as file:
+    #     json_data = json.load(file)
+    #     for data in json_data:
+    #         postId = data['postId']
+    #         media_info = data['extension']['mediaInfo']
+    #         if chat := media_info.get('chat'):
+    #             posts.append((chat['chatId'], data))
+    #             # artist_msgs = chat['artistMessages']
+    #             # if len(artist_msgs['data']):
+    #             #     posts.append((chat['chatId'], data))
+    #         else:
+    #             print('No chat id?', postId, data['shareUrl'])
+    #             break
+    #
+    # '/chat/v1.0/chat-N1XTf9/messages?after=1733566168468%2C4b0a80d517a7229bf3747c89dc04e457&limit=50'
+    # '/chat/v1.0/chat-N1VwSq/messages?limit=50&appId=be4d79eb8fc7bd008ee82c8ec4ff6fd4&language=en&os=WEB&platform=WEB&wpf=pc&wmsgpad=1736165949156&wmd=gvpNwpjvWqh1sc1eiuqDk4eqeDA%3D'
+    # '/chat/v1.0/chat-N1XTf9/messages?after=1733566168468%2C4b0a80d517a7229bf3747c89dc04e457&limit=50&appId=be4d79eb8fc7bd008ee82c8ec4ff6fd4&language=en&os=WEB&platform=WEB&wpf=pc&wmsgpad=1736165384857&wmd=4CuFyOOaJ7JnTQ0PbDxo5uPDUmI%3D'
+    #
+    # for chatId, data in posts:
+    #     postId = data['postId']
+    #     req = f'/chat/v1.0/chat-{chatId}/messages?limit=1000'
+    #     print(req, postId, data['shareUrl'])
+    #     write_all_requests(req, req, f'{output_folder}/{postId}', True, True)
+    #     # break
 
 
 def get_artists(community_id):
@@ -374,7 +414,7 @@ def process_lives(community_id):
     """
     Process the lives https://weverse.io/fromis9/live
     """
-    write_live_tab_posts(community_id)
+    # write_live_tab_posts(community_id)
     write_individual_posts(LIVE_POSTS_JSON, LIVE_POSTS_FOLDER, WRITE_LIVE_COMMENTS)
 
 
@@ -444,8 +484,8 @@ def process_dms():
 
 
 def download():
-    process_members()
-    # process_lives(COMMUNITY_ID)
+    # process_members()
+    process_lives(COMMUNITY_ID)
     # process_artist_posts(COMMUNITY_ID)
     # process_dms()
 
